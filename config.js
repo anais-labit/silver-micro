a7bcac5c7efd03d2d0b42b76557a28fdc3ff1362
jeremy <jeremy.nowak@laplateforme.io>
2024-04-05 09:53:00 +0200

    solving problem

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: 3000,
-    jwtSecret: '!!CryptoCat@!!',
-    jwtExpirationInSeconds: 60 * 60, // 1 hour
-    roles: {
-      USER: 'user',
-      ADMIN: 'admin'
-    }
-}
