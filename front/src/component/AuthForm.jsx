a815ecbc20b10de266c8ea886c754471189fd1de
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2024-04-23 11:21:57 +0200

    switch form auth

--- front/src/component/AuthForm.jsx
+++ front/src/component/AuthForm.jsx
@@ -1,52 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormInput from './FormInput';
 import ValidateButton from './ValidateButton';
 import userImg from '../assets/user.png';
 import lockImg from '../assets/lock.png';
 
-
 export default function AuthForm() {
-    return React.createElement("div", {
-        className: "flex flex-col items-center justify-center w-full h-full space-y-4 bg-white rounded-t-xl"
-    }, 
-        React.createElement("form", {
-            className: "flex flex-col items-center space-y-2"
-        }, 
-            React.createElement("div", {
-                className: "flex flex-row items-center space-y-2 relative"
-            },
-                React.createElement("img", { className:"absolute right-1 bottom-2", src: userImg, alt: "User" }), // Image "user.png" incluse dans le div
-                React.createElement(FormInput, {
-                    placeholder: "Email" })
-            ),
-            React.createElement("div", {
-                className: "flex flex-row items-center space-y-2 relative"
-            },
-                React.createElement("img", { className:"absolute right-1 bottom-2", src: lockImg, alt: "User" }), // Image "user.png" incluse dans le div
-                React.createElement(FormInput, {
-                    placeholder: "Password" })
-            ),
-            React.createElement("div", {
-                className: "flex items-center space-y-4 w-full"
-            }, 
-                React.createElement(ValidateButton, {
-                    className: "space-y-2",
-                    label: "Login"
-                })
-            ),
-            React.createElement("div", {
-                className: "flex flex-row items-center space-x-2 w-full"
-            }, 
-            React.createElement("p", {
-                className: "text-xs text-gray-500"
-            }, "Don't have an account yet ?"), 
-            React.createElement("a", {
-                className: "text-xs text-blue-500"
-            }, "Sign up")
-               
-            ),
+    const [showSignUp, setShowSignUp] = useState(false);
+    const [showSignIn, setShowSignIn] = useState(true);
+
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full space-y-4 bg-white rounded-t-xl">
+            {showSignUp && (
+                <form className="flex flex-col items-center space-y-2">
+                <div className="flex flex-row items-center space-y-2 relative">
+                    <img className="absolute right-1 bottom-2" src={userImg} alt="User" />
+                    <FormInput placeholder="Email" />
+                </div>
+                <div className="flex flex-row items-center space-y-2 relative">
+                    <img className="absolute right-1 bottom-2" src={lockImg} alt="Password" />
+                    <FormInput placeholder="Password" />
+                </div>
+                <div className="flex items-center space-y-4 w-full pt-2">
+                    <ValidateButton className="space-y-2" label="Sign up" />
+                </div>
+                <div className="flex flex-row items-center justify-center space-x-2 w-full pt-6">
+                    <p className="text-xs text-gray-500">Have an account ?</p>
+                    <a className="text-xs text-blue-500" onClick={() => { setShowSignUp(false); setShowSignIn(true); }}>Sign in</a>
+                </div>
+                </form>
+            )}
+            {showSignIn && (
+                <form className="flex flex-col items-center space-y-2">
+                    <div className="flex flex-row items-center space-y-2 relative">
+                    <img className="absolute right-1 bottom-2" src={userImg} alt="User" />
+                    <FormInput placeholder="Email" />
+                </div>
+                <div className="flex flex-row items-center space-y-2 relative">
+                    <img className="absolute right-1 bottom-2" src={lockImg} alt="Password" />
+                    <FormInput placeholder="Password" />
+                </div>
+                <div className="flex items-center space-y-4 w-full pt-2">
+                    <ValidateButton className="space-y-2" label="Sign in" />
+                </div>
+                <div className="flex flex-row items-center justify-center space-x-2 w-full pt-6">
+                    <p className="text-xs text-gray-500">Don't have an account yet ?</p>
+                    <a className="text-xs text-blue-500 underline" onClick={() => { setShowSignUp(true); setShowSignIn(false); }}>Sign up</a>
+                </div>
             
-        )
-                
+                </form>
+            )}
+        </div>
     );
 }
