cdb5d1e47d697088b1422af3c9710c5d24405b0a
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2024-04-24 11:27:53 +0200

    pagination

---
+++ front/src/pages/Authentication.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import AuthForm from '../components/AuthForm'
+import banner from '../assets/banner.jpg'
+
+
+export default function Authentication() {
+    return (
+    <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center flex-col max-[430px]:h-screen lg:h-screen bg-black max-[430px]:border-0 lg:border-2 border-gray-300 max-[430px]:w-full lg:w-1/4'>
+        <div className='h-3/5 flex w-full justify-center items-center'>
+          <img src={banner} alt='banner' className='max-[430px]:w-full lg:w-full h-full brightness-125' />
+        </div>
+        <div className='flex h-full max-[430px]:w-screen lg:w-full justify-center items-center rounded-t-xl'>
+          <AuthForm />
+        </div>
+      </div>
+    </div>
+    )
+}
