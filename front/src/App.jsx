fd95c1196a2a39d5fca1c59978962c46d1f38a33
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2024-05-14 16:24:29 +0200

    root panel

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,6 +7,7 @@ import RestaurantDetails from "./pages/restaurant/RestaurantDetails";
 import CreateRestaurant from "./pages/root/CreateRestaurant";
 import CreateUser from "./pages/root/CreateUser";
 import RootPanel from "./pages/root/RootPanel";
+import RestaurantBookList from "./pages/restaurant/RestaurantBookList";
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
         <Route path="/" element={<Authentication />} />
         <Route path="/restaurants" element={<RestaurantsList />} />
         <Route path="/restaurants/:title" element={<RestaurantDetails />} />
+        <Route path="/restaurants/:title/books" element={<RestaurantBookList />} />
         <Route path="/root/panel" element={<RootPanel />} />
         {/* <Route path="/root/panel/restaurants" element={< RootPanel />} />
       <Route path="/root/panel/users" element={< RootPanel/>} /> */}
-        {/* <Route path="/root/panel/bookings" element={< RootPanel/>} /> */}
+        <Route path="/root/panel/bookings" element={< RootPanel/>} />
         <Route path="/root/panel/restaurants/create" element={<CreateRestaurant />}/>
         <Route path="/root/panel/users/create" element={<CreateUser />} />
         
