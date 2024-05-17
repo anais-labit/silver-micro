b7eefc582c19b2f189d1a5120fb4932c2de6135c
jeremy <jeremy.nowak@laplateforme.io>
2024-05-18 00:01:58 +0200

    display restaurant ok

--- front/src/pages/restaurant/RestaurantsList.jsx
+++ front/src/pages/restaurant/RestaurantsList.jsx
@@ -4,40 +4,47 @@ import bellavita from "../../assets/restoimg/bellavita.jpg";
 import traditions from "../../assets/restoimg/traditions.jpg";
 import { Link } from "react-router-dom";
 import UserMenu from "../../components/UserMenu";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const PATH = import.meta.env.VITE_PATH;
 
+const imageMapping = {
+  "Passe-temps": passetemps,
+  "Bellavita": bellavita,
+};
 
 export default function RestaurantsList() {
-
+  const [restaurantsData, setrestaurantsData] = useState([]);
 
   useEffect(() => {
     const jwtToken = localStorage.getItem("jwtToken");
 
     if (jwtToken) {
-      fetch(PATH + "/pannel/restaurants", {
+      fetch(PATH + "/user/restaurants", {
         headers: {
           Authorization: `Bearer ${jwtToken}`,
         },
       })
         .then((response) => {
           if (!response.ok) {
-            throw new Error("Error fetching resaurants details");
+            throw new Error(
+              `Error fetching restaurants details: ${response.status} ${response.statusText}`
+            );
           }
           return response.json();
         })
         .then((data) => {
-          console.log("Fetch result:", data);
-          // const userData = data.data;
-          // setUserDetails(userData);
+          const fetchData = data.data;
+          console.log("Fetch result:", fetchData);
+          setrestaurantsData(fetchData);
         })
-        .catch((error) => console.error("Error fetching resaurants details:", error));
+        .catch((error) =>
+          console.error("Error fetching restaurants details:", error)
+        );
+    } else {
+      console.error("JWT token is not available in localStorage");
     }
   }, []);
-
-
-
 
   return (
     <section>
@@ -47,73 +54,26 @@ export default function RestaurantsList() {
       <div className="flex justify-center items-center m-10">
         <h2 className="text-5xl font-bold">Les HappyRest'O</h2>
       </div>
-      <div className="flex justify-center items-center m-10 max-sm:flex-col">
-        <div key="passe-temps">
-          <Link to="/restaurants/passe-temps">
-            <Card
-              title="le passe-temps"
-              img={passetemps}
-              adresse="65 bd de Paris"
-              codePostal="13003"
-            />
-          </Link>
-        </div>
 
-        <div key="bellavita">
-          <Link to="/restaurants/bellavita">
-            <Card
-              title="bellavita"
-              img={bellavita}
-              adresse="65 bd de Paris"
-              codePostal="13003"
-            />
-          </Link>
-        </div>
+      {restaurantsData.map((restaurant) => {
+        console.log(restaurant.name);
 
-        <div key="traditions">
-          <Link to="/restaurants/traditions">
-            <Card
-              title="traditions"
-              img={traditions}
-              adresse="65 bd de Paris"
-              codePostal="13003"
-            />
-          </Link>
-        </div>
+        const restaurantImage = imageMapping[restaurant.name] || passetemps; 
 
-        <div key="resto4">
-          <Link to="/restaurants/resto4">
-            <Card
-              title="le passe-temps"
-              img={passetemps}
-              adresse="65 bd de Paris"
-              codePostal="13003"
-            />
-          </Link>
-        </div>
-
-        <div key="resto5">
-          <Link to="/restaurants/resto5">
-            <Card
-              title="le passe-temps"
-              img={passetemps}
-              adresse="65 bd de Paris"
-              codePostal="13003"
-            />
-          </Link>
-        </div>
-
-        <div key="resto6">
-          <Link to="/restaurants/resto6">
-            <Card
-              title="le passe-temps"
-              img={passetemps}
-              adresse="65 bd de Paris"
-              codePostal="13003"
-            />
-          </Link>
-        </div>
-      </div>
+        return (
+          <div className="flex justify-center items-center m-10 max-sm:flex-col" key={restaurant.id}>
+            <div>
+              <Link to={`/restaurants/${restaurant.slug}`}>
+                <Card
+                  title={restaurant.name}
+                  img={restaurantImage}
+                  adresse={restaurant.address}
+                />
+              </Link>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 }
