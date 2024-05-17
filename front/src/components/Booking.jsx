ddc0598c2a3ed3b6baaaf891bc917d1fbd5555af
Guangquan Ye <shinz@mbpdeguangquan.home>
2024-05-18 00:01:25 +0200

    probleme sur le recap fix

--- front/src/components/Booking.jsx
+++ front/src/components/Booking.jsx
@@ -15,23 +15,10 @@ export default function Booking() {
     const [selectedPax, setSelectedPax] = useState(null);
     const [isConfirmed, setIsConfirmed] = useState(false);
     const [bookedPax, setBookedPax] = useState();
+    const [confirmationDate, setConfirmationDate] = useState(null);
 
-    const jours = ["lun", "mar", "mer", "jeu", "ven", "sam", "dim"];
-
-    const heures = [
-        "12:00",
-        "12:30",
-        "13:00",
-        "13:30",
-        "14:00",
-        "19:00",
-        "19:30",
-        "20:00",
-        "20:30",
-        "21:00",
-        "21:30",
-    ];
-
+    const jours = ["dim", "lun", "mar", "mer", "jeu", "ven", "sam"];
+    const heures = ["12:00", "12:30", "13:00", "13:30", "14:00", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30"];
     const pax = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
     useEffect(() => {
@@ -48,7 +35,6 @@ export default function Booking() {
                 const data = await response.json();
                 if (data && data.status) {
                     const restaurantId = data.data.id;
-
                     setRestaurantId(restaurantId);
 
                     const bookingsResponse = await fetch(
@@ -63,7 +49,6 @@ export default function Booking() {
 
                     const bookingsData = await bookingsResponse.json();
                     console.log("Restaurant Bookings:", bookingsData.data);
-                    
 
                     if (!bookingsResponse.ok) {
                         throw new Error("Error fetching restaurant bookings");
@@ -90,10 +75,7 @@ export default function Booking() {
     };
 
     const handleButtonClick = (state) => {
-        if (
-            (state === "HEURES" && displayState === "JOURS") ||
-            (state === "pax" && displayState === "HEURES")
-        ) {
+        if ((state === "HEURES" && displayState === "JOURS") || (state === "pax" && displayState === "HEURES")) {
             setSelectedHour(null);
             setSelectedPax(null);
         }
@@ -107,22 +89,13 @@ export default function Booking() {
         e.preventDefault();
 
         const datePlusHour = () => {
-
-            const datePart = selectedDate.toLocaleString("default");
-
-            const dateOnly = datePart.split(' ')[0];
-
-            const convertedDate = dateOnly.split('/');
-
-            const concatenatedDates = `${convertedDate[2]}-${convertedDate[1]}-${convertedDate[0]}`;
-
-            const datePlusHour = `${concatenatedDates}T${selectedHour}:00.000Z`;
+            const datePart = selectedDate.toLocaleDateString("fr-CA"); // Format date to "YYYY-MM-DD"
+            const datePlusHour = `${datePart}T${selectedHour}:00.000Z`;
             setBookedHours(selectedHour);
             return datePlusHour;
-        }
+        };
 
         if (selectedDate && selectedHour && selectedPax) {
-
             const bookingPayload = {
                 id_user: localStorage.getItem("id"),
                 id_restaurant: restaurantId,
@@ -133,17 +106,14 @@ export default function Booking() {
 
             try {
                 const jwtToken = localStorage.getItem("jwtToken");
-                const response = await fetch(
-                    `${PATH}/user/restaurants/${restaurantId}/books`,
-                    {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: `Bearer ${jwtToken}`,
-                        },
-                        body: JSON.stringify(bookingPayload),
-                    }
-                );
+                const response = await fetch(`${PATH}/user/restaurants/${restaurantId}/books`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${jwtToken}`,
+                    },
+                    body: JSON.stringify(bookingPayload),
+                });
 
                 const result = await response.json();
                 if (result.status === false) {
@@ -151,6 +121,7 @@ export default function Booking() {
                 }
                 console.log("Booking réussi");
                 setIsConfirmed(true);
+                setConfirmationDate(selectedDate); // Set confirmation date to selected date
                 setSelectedDate(new Date());
                 setSelectedHour(null);
                 setSelectedPax(null);
@@ -159,20 +130,17 @@ export default function Booking() {
                 console.error("Error confirming booking:", error);
             }
         } else {
-            console.log(
-                "Veuillez sélectionner une date, une heure et un nombre de pax."
-            );
+            console.log("Veuillez sélectionner une date, une heure et un nombre de pax.");
         }
     };
 
-    const formatedDate = `${jours[selectedDate.getDay() - 1]
-        } ${selectedDate.getDate()} ${selectedDate.toLocaleString("default", {
-            month: "long",
-        })}`;
+    const formatedDate = `${jours[selectedDate.getDay()]} ${selectedDate.getDate()} ${selectedDate.toLocaleString("default", { month: "long" })}`;
+    const confirmationFormatedDate = confirmationDate
+        ? `${jours[confirmationDate.getDay()]} ${confirmationDate.getDate()} ${confirmationDate.toLocaleString("default", { month: "long" })}`
+        : "";
 
     return (
         <section className="max-sm:w-full lg:w-[420px] lg:rounded-lg h-[490px] bg-white relative shadow-lg">
-
             {!isConfirmed && (
                 <div className="grid grid-cols-3 justify-center items-center gap-4 m-8">
                     <button onClick={() => handleButtonClick("JOURS")}>
@@ -192,66 +160,45 @@ export default function Booking() {
                     </button>
                 </div>
             )}
-
             {!isConfirmed && displayState === "JOURS" && (
                 <div className="m-5 mt-12 flex justify-center">
-                    <Calendar
-                        onChange={(date) => handleDayClick(date)}
-                        value={selectedDate}
-                    />
+                    <Calendar onChange={(date) => handleDayClick(date)} value={selectedDate} />
                 </div>
             )}
-
             {!isConfirmed && displayState === "HEURES" && (
                 <div className="m-5 mt-16">
                     <div className="grid grid-cols-3 gap-4">
                         {heures.map((heure, heureIndex) => (
-                            <button
-                                className="border-black border-2"
-                                onClick={() => handleHourClick(heure)}
-                                key={heureIndex}
-                            >
+                            <button className="border-black border-2" onClick={() => handleHourClick(heure)} key={heureIndex}>
                                 {heure}
                             </button>
                         ))}
                     </div>
                 </div>
             )}
-
             {!isConfirmed && displayState === "pax" && (
                 <div className="m-5 mt-16">
                     <div className="m-5 grid grid-cols-4 gap-4">
                         {pax.map((personne, personneIndex) => (
-                            <button
-                                className="border-black border-2"
-                                onClick={() => setSelectedPax(personne)}
-                                key={personneIndex}
-                            >
+                            <button className="border-black border-2" onClick={() => setSelectedPax(personne)} key={personneIndex}>
                                 {personne}
                             </button>
                         ))}
                     </div>
                 </div>
             )}
-
             {isConfirmed && (
                 <div className="p-5 h-full">
                     <div className="flex font-bold text-xl flex-col h-full justify-center">
-                        <p className="text-center ">
-                            Réservation confirmée pour {bookedPax} personne(s) le{" "}
-                            {formatedDate} à {bookedHours}.
-
+                        <p className="text-center">
+                            Réservation confirmée pour {bookedPax} personne(s) le {confirmationFormatedDate} à {bookedHours}.
                         </p>
                     </div>
                 </div>
             )}
-
             {!isConfirmed && selectedDate && selectedHour && selectedPax && (
                 <div className="absolute p-5 inset-x-0 bottom-0">
-                    <button
-                        onClick={handleBookingConfirm}
-                        className="bg-black px-3 pb-1 text-white rounded-xl text-xl w-full"
-                    >
+                    <button onClick={handleBookingConfirm} className="bg-black px-3 pb-1 text-white rounded-xl text-xl w-full">
                         Confirmer la réservation
                     </button>
                 </div>
