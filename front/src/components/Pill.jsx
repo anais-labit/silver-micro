28cb1c502221cb4cf8062a08039b2b9ec1b05cb5
jeremy <jeremy.nowak@laplateforme.io>
2024-05-18 11:51:34 +0200

    Pills OK

--- front/src/components/Pill.jsx
+++ front/src/components/Pill.jsx
@@ -1,9 +1,13 @@
 export default function Pill({ tags }) {
-
-
-    return (
-        <div className="flex flex-row bg-black rounded-full justify-center items-center px-3 py-1">
-        <p className=" text-white text-lg font-extrabold " >{tags}</p>
-        </div>  
-    );
-    }
+  return (
+    <>
+      {tags.map((tag, index) => (
+        <div className="flex flex-row bg-black rounded-md mt-3 justify-center items-center px-3 py-1">
+          <p key={index} className="text-white text-xs font-extrabold ">
+            {tag}
+          </p>
+        </div>
+      ))}
+    </>
+  );
+}
